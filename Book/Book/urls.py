00d09663e4324fc1eea2e0be31ce0866a0c74ab9
Pontaaaa <jonathan.stenstrom@hotmail.com>
2024-02-20 01:09:59 +0100

    add sign-up and sign-in feature

--- Book/Book/urls.py
+++ Book/Book/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("Rent.urls")),
+    path('signin/', views.sign_in_modal, name='signin'),
+    path('signup/', views.sign_up_modal, name='signup'),
 ]
---
+++ Book/Book/views.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+
+def sign_in_modal(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'sign_in_modal.html', {'form': form})
+
+def sign_up_modal(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'sign_up_modal.html', {'form': form})
